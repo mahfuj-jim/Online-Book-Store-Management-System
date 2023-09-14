b97d7f34f80d8be17f163a64f2a72753fd6dba73
mahfuj-jim <mahfuj.ahmed@bjitacademy.com>
2023-09-14 15:39:10 +0600

    Added Schema for both Auth & User Model

---
+++ models/user_model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is not provided"],
+      maxLength: 30,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is not provided"],
+      validate: {
+        validator: function (value) {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return emailRegex.test(value);
+        },
+        message: "Invalid email format",
+      },
+    },
+    phoneNumber: {
+      type: String,
+      unique: true,
+      required: [true, "Phone Number is not provided"],
+      validate: {
+        validator: function (value) {
+          const phoneNumberRegex = /^\d{11}$/;
+          return phoneNumberRegex.test(value);
+        },
+        message: "Invalid phone number format (must be 11 digits)",
+      },
+    },
+    address: {
+      district: {
+        type: String,
+        required: [true, "District is required"],
+      },
+      area: {
+        type: String,
+        required: [true, "Area is required"],
+      },
+      houseNumber: {
+        type: String,
+        required: [true, "House Number is required"],
+      },
+      additionalInformation: {
+        type: String,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel = mongoose.model("users", userSchema);
+
+module.exports = UserModel;
